a51039927130d12fe78f2ebfd3c6a0151cfc06c9
Musabbir Hossain Chowdhury <56753519+Musabbir2@users.noreply.github.com>
2022-06-01 00:46:28 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace Inheritance
+{
+    class Branch
+    {
+        int BranchCode;
+        string BranchName, BranchAddress;
+        public void GetBranchData()
+        {
+            Console.WriteLine("Enter your Branch Details:-");
+            Console.WriteLine("Enter your Branch code:");
+            BranchCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Branch Name:");
+            BranchName= Console.ReadLine();
+            Console.WriteLine("Enter your Branch Address:");
+            BranchAddress = Console.ReadLine();
+        }
+        public void DisplayBranchData()
+        {
+            Console.WriteLine($"Your Branch code is: {BranchCode}");
+            Console.WriteLine($"Your Branch Name is: {BranchName}");
+            Console.WriteLine($"Your Branch Address is: {BranchAddress}");
+        }
+    }
+
+    class Employee : Branch
+    {
+        int EmployeeId, EmployeeAge;
+        string EmployeeName, EmployeeAddress;
+        public void GetEmployeeData()
+        {
+            Console.WriteLine("Enter Employee Details:-");
+            Console.WriteLine("Enter Employee Id:");
+            EmployeeId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name:");
+            EmployeeName = Console.ReadLine();
+            Console.WriteLine("Enter age:");
+            EmployeeAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Address:");
+            EmployeeAddress = Console.ReadLine();
+        }
+        public void DisplayEmployeeData()
+        {
+            Console.WriteLine($"Employee ID is:{EmployeeId}");
+            Console.WriteLine($"Employee Name is:{EmployeeName}");
+            Console.WriteLine($"Employee Age is:{EmployeeAge}");
+            Console.WriteLine($"Employee Address is:{EmployeeAddress}");
+        }
+    }
+
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Employee x = new Employee();
+            x.GetBranchData();
+            x.DisplayBranchData();
+            x.GetEmployeeData();
+            x.DisplayEmployeeData();
+            Console.WriteLine("Press Any key to Exit");
+            Console.ReadKey();
+        }
+    }
+
+}
